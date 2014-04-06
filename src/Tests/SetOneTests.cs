d92122a9f6a101b95d02d63d4c4fbeae83d91d1a
Wesley McClure <wes.mcclure@gmail.com>
2014-04-05 20:23:48 -0700

    service example - two parallel categories of tests

---
+++ src/Tests/SetOneTests.cs
@@ -0,0 +1,15 @@
+﻿namespace Tests
+{
+	using NUnit.Framework;
+
+	[TestFixture]
+	[Category("SetOne")]
+	public class SetOneTests : AssertionHelper
+	{
+		[Test]
+		public void Test()
+		{
+			Expect(true);
+		}
+	}
+}
---
+++ src/Tests/SetTwoTests.cs
@@ -0,0 +1,15 @@
+﻿namespace Tests
+{
+	using NUnit.Framework;
+
+	[TestFixture]
+	[Category("SetTwo")]
+	public class SetTwoTests : AssertionHelper
+	{
+		[Test]
+		public void Test()
+		{
+			Expect(true);
+		}
+	}
+}
